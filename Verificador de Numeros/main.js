2371438c0b7aadbf9694acb33a4841ee10a3f367
Rafael do Amaral Costa <104049906+jake7038@users.noreply.github.com>
2024-06-19 21:21:13 -0300

    Add files via upload

---
+++ Verificador de Numeros/main.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('form-deposito');
+function campoVazio(x, y){
+    if(x != null && y != null)
+    return true;
+}
+
+form.addEventListener("submit", function(e){
+    let formEValido = false;
+    e.preventDefault();
+    
+    var primeiroNumero = document.getElementById("primeiro-numero");
+    var segundoNumero = document.getElementById("segundo-numero");
+    
+    var resultado = 'Não sei';
+
+    
+    
+    
+    formEValido = campoVazio(primeiroNumero.value, segundoNumero.value);
+
+   
+    if(formEValido){
+       switch (primeiroNumero.value > segundoNumero.value){
+        case true:
+        resultado = "Maior que";
+        break;
+        case false:
+            switch (segundoNumero.value == primeiroNumero.value){
+                case false:
+                    resultado = "Menor que";
+                    break;
+                case true:
+                    resultado = "Igual";
+                    break;
+            }
+
+       }
+        const mensagemSucesso = `o Número <b>${primeiroNumero.value}</b> é <b> ${resultado}</b> o numero <b>${segundoNumero.value}</b>`;
+        document.querySelector('.success-message').innerHTML = mensagemSucesso;
+            const containerMesagemSucesso = document.querySelector(".success-message");
+            containerMesagemSucesso.innerHTML = mensagemSucesso;
+            containerMesagemSucesso.style.display = 'block';
+            primeiroNumero.value='';
+            segundoNumero.value='';
+    } 
+    
+    else{
+        alert("Confira os campos novamente");
+    }
+})
+console.log(form);
