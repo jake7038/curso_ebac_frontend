1061f099ebd6fc742941c96fee0490a8724723f8
Rafael do Amaral Costa <104049906+jake7038@users.noreply.github.com>
2024-08-05 22:07:17 -0300

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,34 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass')(require('sass'));
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemiin');
 
 function compilaSass(){
     return gulp.src('./source/styles/main.scss')
+    .pipe(sourcemaps.init())
     .pipe(sass({
         outputStyle:   'compressed'
     }))
+    .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./build/styles'));
 }
 
 
+function comprimirImagem(){
+    return gulp.src('./source/imagens/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/img'))
+}
+
+
+function comprimejs(){
+    return gulp.src('./source/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./build/scripts'))
+}
+
+
 exports.default = compilaSass;
+exports.javascripts = comprimejs;
+exports.comprimirimagem = comprimirImagem;
