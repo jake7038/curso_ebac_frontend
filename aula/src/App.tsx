5ccabb7f62991ffc961d0cbb2eb97240f41f6976
rafa <rafaelcostatricolor@gmail.com>
2024-10-06 19:52:09 -0300

    exercicio

---
+++ aula/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+import Test from './teste'
+
+function App() {
+  return (
+    <Test />
+  );
+}
+
+export default App;
---
+++ aula/src/teste.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const Botao = styled.button`
+  background-color: red;
+`
+
+function Teste(){
+  return <Botao><Botao/>
+}
+
+export default Teste
